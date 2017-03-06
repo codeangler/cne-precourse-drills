8b078770ad5b658a0c3112bdda5f08da1abc34fc
Casey Burnett <codeangler@users.noreply.github.com>
2017-03-06 14:52:06 -0700

    Create scratchpad.java

---
+++ scratchpad.java
@@ -0,0 +1,40 @@
+/* File name : EncapTest.java */
+public class Address {
+   private String street;
+   private String city;
+   private String state;
+   private String zip;
+
+
+   public int getStreet() {
+      return street;
+   }
+
+   public String getCity() {
+      return city;
+   }
+   
+   public String getState() {
+      return state;
+   }
+   
+   public String getZip() {
+      return zip;
+   }
+
+   public String getIdNum() {
+      return idNum;
+   }
+
+   public void setAge( int newAge) {
+      age = newAge;
+   }
+
+   public void setName(String newName) {
+      name = newName;
+   }
+
+   public void setIdNum( String newId) {
+      idNum = newId;
+   }
+}
